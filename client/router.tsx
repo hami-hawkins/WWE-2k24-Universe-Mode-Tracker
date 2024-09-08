062fc805b44a5ab2aa4cb962d987f2e58161db8c
Hami Hawkins <hhaami@gmail.com>
2024-09-09 11:32:47 +1200

    Set up routing for Draft and AutoDraft

--- client/router.tsx
+++ client/router.tsx
@@ -5,9 +5,18 @@ import {
   Route,
 } from 'react-router-dom'
 import Homepage from './components/Homepage'
+import Draft from './components/Draft'
+import AutoDraft from './components/AutoDraft'
 
 const router = createBrowserRouter(
-  createRoutesFromElements([<Route path="/" element={<Homepage />} />]),
+  createRoutesFromElements([
+    <>
+      <Route path="/" element={<Homepage />} />
+      {/* Add routes below here */}
+      <Route path="/draft" element={<Draft />} />
+      <Route path="/autodraft" element={<AutoDraft />} />
+    </>,
+  ]),
 )
 
 export default router
