1a82a29656a758e57cea5a0e54cc97bdc33bcbe6
Hami Hawkins <hhaami@gmail.com>
2024-09-09 11:41:03 +1200

    Add draft api calls

--- client/apis/wrestlersApi.ts
+++ client/apis/wrestlersApi.ts
@@ -24,9 +24,29 @@ export async function deleteWrestler(id: number) {
 }
 
 //draft a wrestler to raw
+export async function draftToRaw(id: number) {
+  return await request
+    .patch(`/api/v1/wrestlers/brand/${id}`)
+    .send({ brand: 'Raw' })
+}
 
 //draft a wrestler to smackdown
+export async function draftToSmackdown(id: number) {
+  return await request
+    .patch(`/api/v1/wrestlers/brand/${id}`)
+    .send({ brand: 'Smackdown' })
+}
 
 //draft a wrestler to nxt
+export async function draftToNXT(id: number) {
+  return await request
+    .patch(`/api/v1/wrestlers/brand/${id}`)
+    .send({ brand: 'NXT' })
+}
 
 //undraft wrestler
+export async function undraftWrestler(id: number) {
+  return await request
+    .patch(`/api/v1/wrestlers/brand/${id}`)
+    .send({ brand: '' })
+}
